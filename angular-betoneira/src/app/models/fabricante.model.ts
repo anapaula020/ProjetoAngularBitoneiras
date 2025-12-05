b056451b5fa1d40337839e325d1124f98eb8013e
Michael Silva Sena <33912376+slonyrufus@users.noreply.github.com>
2025-12-05 16:15:31 -0300

    Update fabricante.model.ts

--- angular-betoneira/src/app/models/fabricante.model.ts
+++ angular-betoneira/src/app/models/fabricante.model.ts
@@ -1,9 +1,11 @@
 export class Fabricante {
     id!: number;
     nome!: string;
+    cnpj!: string;
 
-    constructor(id: number,nome: string,) {
+    constructor(id: number,nome: string,cnpj: string) {
         this.id = id;
         this.nome = nome;
+        this.cnpj = cnpj;
     }
 }
